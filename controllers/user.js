b98019a46176f22dbeb5c0f688aeb675f2916082
gpla <germanps@outlook.es>
2020-03-11 08:32:54 +0100

    Des/Activate user endpoint

--- controllers/user.js
+++ controllers/user.js
@@ -208,6 +208,27 @@ async function updateUser(req, res){
     });
 }
 
+function activateUser(req, res) {
+    const { id } = req.params;
+    const { active } = req.body;
+    
+    User.findByIdAndUpdate(id, { active: active }, (err, userStored) => {
+        if (err) {
+            res.status(500).send({ message: "Error de servidor."});
+        }else{
+            if (!userStored) {
+                res.status(404).send({ message: "Usuario no encontrado."});
+            }else{
+                if(active === true){
+                    res.status(200).send({ message: "Usuario activado correctamente."});
+                }else{
+                    res.status(200).send({ message: "Usuario desactivado correctamente."});
+                }
+            }
+        }
+    });
+}
+
 
 
 
@@ -218,5 +239,6 @@ module.exports = {
     getUsersActive,
     uploadAvatar,
     getAvatar,
-    updateUser
+    updateUser,
+    activateUser
 } 
